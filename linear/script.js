026e2354c48a4794a95b841b3840644e817baae5
lucasdefino <ldefino@mail.utdt.edu>
2023-04-17 19:18:38 -0300

    Lunes 17/04

--- linear/script.js
+++ linear/script.js
@@ -1,11 +1,9 @@
-d3.dsv(';', '147_01-07_enero2.csv', d3.autoType).then(data => {
+d3.dsv(';', '147_15-21_junio.csv', d3.autoType).then(data => {
 
   let chart = Plot.plot({
-    //facet: {data: data,y: "categoria",marginRight: 140},
     marks: [
-      //Plot.areaY(data.filter(d => d.categoria === 'ALUMBRADO'), {x: "hora_ingreso",y: 'tiempo_de_cierre', fillOpacity: 1}),
-      Plot.lineY(data.filter(d => d.categoria === 'ALUMBRADO'), {x: "hora_ingreso",y: 'tiempo_de_cierre', strokeWidth: 1})
-    ],
-  })
+      Plot.lineY(data, Plot.groupX({y: "count"}, {x: "fecha_ingreso"})),
+     ]
+    })
   d3.select('#chart').append(() => chart)
 })
