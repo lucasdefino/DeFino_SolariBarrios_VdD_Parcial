0ef3e3c3d7dca64cae95f44eb73a711215814187
lucasdefino <lucasdefino12@gmail.com>
2023-04-25 00:55:20 -0300

    cnala y mapa

--- canal/script.js
+++ canal/script.js
@@ -1,13 +1,13 @@
 d3.dsv(';', '147_18-24_agosto20.csv', d3.autoType).then(data => {
 
-  let datafiltered = data.filter((d => d.domicilio_barrio == 'VILLA URQUIZA' || d.domicilio_barrio == 'VILLA PUEYRREDON' || d.domicilio_barrio == 'FLORES' || d.domicilio_barrio == 'BARRACAS')) //&& (d => d.canal == 'App BA 147' || d.canal == 'GCS Web'|| d.canal == 'Operador UGIS'|| d.canal == 'App Denuncia Vial'))
+  let datafiltered = data.filter((d => d.domicilio_barrio == 'VILLA URQUIZA' || d.domicilio_barrio == 'VILLA PUEYRREDON' || d.domicilio_barrio == 'CABALLITO' || d.domicilio_barrio == 'BARRACAS')) //&& (d => d.canal == 'App BA 147' || d.canal == 'GCS Web'|| d.canal == 'Operador UGIS'|| d.canal == 'App Denuncia Vial'))
   
   let chart = Plot.plot({
     height:500,
     //width:800,
     color: {
-      //legend: true,
-      range: ['#236865','#2BA193','#8AB37F','#E76F51','#E9C46A','#E55E3D','#E24D28']
+      legend: true,
+      range: ['#236865','#2BA193','#8AB37F','#E9C46A','#E55E3D','#E24D28','#E76F51','#E24D28']
     },
     marks: [
       Plot.barY(datafiltered,Plot.groupX({y: "count"}, {x: 'domicilio_barrio', fill:'canal',opacity: 0.8, stroke:'canal', strokeWidth: 4})),
@@ -25,7 +25,9 @@ d3.dsv(';', '147_18-24_agosto20.csv', d3.autoType).then(data => {
       backgroundColor: '#1B2F37',
       color: '#2BA193',
     },
-    
+    legend: {
+      
+    },
     })
   
   d3.select('#chart').append(() => chart)
