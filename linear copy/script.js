f519ef3ee81d8ae09ff3eb211f8d7340d3e3e194
lucasdefino <lucasdefino12@gmail.com>
2023-04-25 18:47:41 -0300

    dudoso

--- linear copy/script.js
+++ linear copy/script.js
@@ -1,17 +1,11 @@
 const data = d3.dsv(';', 'graficojunio.csv', d3.autoType).then(data => {
 
-  let datafilteredE = data.filter(d => d.mes == 'ENERO')
-  let datafilteredJ = data.filter(d => d.mes == 'JUNIO')
-  let datafilteredA = data.filter(d => d.mes == 'AGOSTO')
-
   let chart = Plot.plot({
     
-    height:600,
+    //height:600,
     width:1000,
     marginBottom:50,
-    marginRight:210,
-    marginLeft:70,
-    
+    marginRight:110,
     
     facet: {
       data:data,
@@ -20,18 +14,55 @@ const data = d3.dsv(';', 'graficojunio.csv', d3.autoType).then(data => {
     },
     
     marks: [
-
-      //Plot.areaY(datafilteredE,Plot.groupX({y: "count"}, {x: "hora",fill:'#8AB37F',curve:'basis', opacity: 0.7})),
-      Plot.line(datafilteredE,Plot.groupX({y: "count"}, {x: "hora",curve:'basis', stroke:'#8AB37F', strokeWidth: 2})),  
-
       
-      Plot.areaY(datafilteredJ,Plot.groupX({y: "count"}, {x: "hora",fill: '#E24D28',curve:'basis', opacity: 0.7})),
-      Plot.lineY(datafilteredJ,Plot.groupX({y: "count"}, {x: "hora",curve:'basis', stroke: '#E24D28', strokeWidth: 2})),
-      //Plot.ruleY([0])
+      Plot.areaY(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'ENERO',
+        fill:'#8AB37F',
+        curve:'basis', 
+        opacity: 0.3
+      })),
 
-      //Plot.areaY(datafilteredA,Plot.groupX({y: "count"}, {x: "hora",fill:'#236865',curve:'basis',opacity: 0.7})),
-      Plot.lineY(datafilteredA,Plot.groupX({y: "count"}, {x: "hora",curve:'basis', stroke:'#236865', strokeWidth: 2})),
+      Plot.line(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'ENERO',
+        curve:'basis',
+        stroke:'#8AB37F',
+        strokeWidth: 0.5
+      })),  
 
+      Plot.areaY(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'AGOSTO',
+        fill:'#236865',
+        curve:'basis',
+        opacity: 0.3
+      })),
+
+      Plot.lineY(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'AGOSTO',
+        curve:'basis',
+        stroke:'#236865',
+        strokeWidth: 0.5
+      })),
+
+            
+      Plot.areaY(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'JUNIO',
+        fill: '#E24D28',
+        curve:'basis',
+        opacity: 0.7
+      })),
+      Plot.lineY(data,Plot.groupX({y: "count"}, {
+        x: "hora",
+        filter: (d) => d.mes === 'JUNIO',
+        curve:'basis',
+        stroke: '#E24D28',
+        strokeWidth: 2
+      })),
+      
      ],
      style: {
       backgroundColor: '#1B2F37',
@@ -39,6 +70,7 @@ const data = d3.dsv(';', 'graficojunio.csv', d3.autoType).then(data => {
     },
     y: {
       axis: true,
+      label:'',
     },
     x: {
       ticks: ['00:00','02:00','04:00','06:00','08:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00','23:30'],
