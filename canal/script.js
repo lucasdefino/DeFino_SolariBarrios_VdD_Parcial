2f88c1591e61fea866fa74c6556c5c45fd64c0ef
lucasdefino <ldefino@mail.utdt.edu>
2023-04-25 18:57:21 -0300

    leyendas

--- canal/script.js
+++ canal/script.js
@@ -1,18 +1,17 @@
 d3.dsv(';', '147_18-24_agosto20.csv', d3.autoType).then(data => {
 
-  let datafiltered = data.filter((d => d.domicilio_barrio == 'VILLA URQUIZA' || d.domicilio_barrio == 'VILLA PUEYRREDON' || d.domicilio_barrio == 'CABALLITO' || d.domicilio_barrio == 'BARRACAS')) //&& (d => d.canal == 'App BA 147' || d.canal == 'GCS Web'|| d.canal == 'Operador UGIS'|| d.canal == 'App Denuncia Vial'))
-  
+  let datafiltered = data.filter((d => ((d.domicilio_barrio == 'VILLA URQUIZA' || d.domicilio_barrio == 'VILLA PUEYRREDON' || d.domicilio_barrio == 'CABALLITO' || d.domicilio_barrio == 'BARRACAS') && ( d.canal == 'App BA 147' || d.canal == 'GCS Web'|| d.canal == 'Operador UGIS'|| d.canal == 'App Denuncia Vial' || d.canal == 'Boti'))))
+
   let chart = Plot.plot({
+    marginLeft:0,
+    marginRight:0,
     height:500,
-    //width:800,
     color: {
-      legend: true,
-      range: ['#236865','#2BA193','#8AB37F','#E9C46A','#E55E3D','#E24D28','#E76F51','#E24D28']
+      legend:true,
+      range: ['#236865','#2BA193','#8AB37F','#E55E3D','#E24D28']
     },
     marks: [
-      Plot.barY(datafiltered,Plot.groupX({y: "count"}, {x: 'domicilio_barrio', fill:'canal',opacity: 0.8, stroke:'canal', strokeWidth: 4})),
-      //Plot.text(datafiltered,{datafiltered, Plot.groupX({y: "count"}, {x: 'domicilio_barrio', fill:'canal'}), text: d => (d. * 100).toFixed(1), dy: -5}),
-      //Plot.text(datafiltered,Plot.groupX({y: "count"}, {x: 'domicilio_barrio', fill:'canal',}),{text: d => '${d.y}'}) //{filter: d => d.y, text: d => `${d.y}`})
+      Plot.barY(datafiltered,Plot.groupX({y: "count",text:"first",}, {x: 'domicilio_barrio', fill:'canal',opacity: 0.8, stroke:'canal', strokeWidth: 4,})),
     ],
     y: {
       label: "",
@@ -24,13 +23,21 @@ d3.dsv(';', '147_18-24_agosto20.csv', d3.autoType).then(data => {
     style: {
       backgroundColor: '#1B2F37',
       color: '#2BA193',
-    },
-    legend: {
-      
-    },
+    }
     })
+    let alpha = chart.legend('color',{
+      columns:1,
+      className: "leyenda_canal",
+      swatchSize: 20,
+      style: {
+        fontFamily: "Poppins",
+        fontSize: "20px"
+      }
+    })
+  var svg = d3.select('#chart').append(() => chart)
+  svg.append(() => alpha)
   
-  d3.select('#chart').append(() => chart)
+
 })
 
 //#69b3a2
